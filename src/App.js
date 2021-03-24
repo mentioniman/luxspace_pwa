163d957b1d0704522f8c7fc1aacc29f207337537
cemon <mentioniman@gmail.com>
2021-03-24 15:54:03 +0700

    migrasi dari template ke component react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
+import Browse from "./components/Browse.js";
+import Arrived from "./components/Arrived.js";
+import Clients from "./components/Clients.js";
+import Modal from "./components/Modal.js";
+import AsideMenu from "./components/AsideMenu.js";
+import Footer from "./components/Footer.js";
+import Offline from "./components/Offline.js";
+
+
 
 function App() {
+  const [showModal, setShowModal] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+
+  function handleOfflineStatus() {
+    console.log(!navigator.onLine);
+    setOfflineStatus(!navigator.onLine);
+  }
+  function handleShowModal(event) {
+    setShowModal(!showModal);
+  }
+  React.useEffect(
+    function () {
+      (async function () {
+        const response = await fetch(
+          "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+              "x-api-key": process.env.REACT_APP_APIKEY,
+            },
+          }
+        );
+        const { nodes } = await response.json();
+        setItems(nodes);
+      })();
+
+      handleOfflineStatus();
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
+
+      return function cleanup() {
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
+      };
+    },
+    [offlineStatus]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived  />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+      {showModal && <Modal handleShowModal={handleShowModal} />}
+    </>
   );
 }
 
